n64decomp/perfect_dark

commit 920c0eada56d7c3cce0470a9f96803070acfb10d

Rate limit stuckage RNG for skedar doors
Limits calculation to 15 times per second, close to that to how fast this would execute on original hardware

src/game/propobj.c
@@ -20056,7 +20056,12 @@ bool doorCalcIntendedFrac(struct doorobj *door)
 		if (door->base.flags3 & OBJFLAG3_DOOR_STICKY) {
 			s32 value = (random() % 64) + 30;
 
+#ifndef PLATFORM_N64 // emulate low fps cal rate for stackage test
+			if (((g_Vars.lvframenum % value) == 0)
+				&& ((g_Vars.lvframe60 & 3) == 0)) {
+#else
 			if ((g_Vars.lvframenum % value) == 0) {
+#endif
 				bool dothething = false;
 				struct doorobj *loopdoor;
 

wtfloltk/perfect_dark_ctr

commit 8a757745064d9fb67e6f706178fad3a95fb6f88d

Fix comment typo
Introduced in commit 920c0eada56d7c3cce0470a9f96803070acfb10d

src/game/propobj.c
@@ -20060,7 +20060,7 @@ bool doorCalcIntendedFrac(struct doorobj *door)
 		if (door->base.flags3 & OBJFLAG3_DOOR_STICKY) {
 			s32 value = (random() % 64) + 30;
 
-#ifndef PLATFORM_N64 // emulate low fps cal rate for stackage test
+#ifndef PLATFORM_N64 // emulate low fps cal rate for stuckage test
 			if (((g_Vars.lvframenum % value) == 0)
 				&& ((g_Vars.lvframe60 & 3) == 0)) {
 #else