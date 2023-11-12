eecee9d94054d9885a960a415a79bd7f0ee5c8be
fgsfds <derp.primus@gmail.com>
2023-11-12 13:56:50 +0100

    port: fix #55
    patch submitted by Ryan Dwyer

--- src/game/hudmsg.c
+++ src/game/hudmsg.c
@@ -1515,7 +1515,9 @@ Gfx *hudmsgsRender(Gfx *gdl)
 					gdl = hudmsgRenderBox(gdl, x - 3, y - 3, x + msg->width + 2, y + msg->height + 2, 1.0f, bordercolour, spc0);
 #endif
 
-					gdl = textRenderProjected(gdl, &x, &y, msg->text, msg->font1, msg->font2, textcolour, viGetWidth(), viGetHeight(), 0, 0);
+					if (spc0 > 0) {
+						gdl = textRenderProjected(gdl, &x, &y, msg->text, msg->font1, msg->font2, textcolour, viGetWidth(), viGetHeight(), 0, 0);
+					}
 				} else {
 					gdl = text0f153a34(gdl, x, y, x + msg->width, y + msg->height, 0);
 
@@ -1597,7 +1599,9 @@ Gfx *hudmsgsRender(Gfx *gdl)
 					gdl = hudmsgRenderBox(gdl, x - 3, y - 3, x + msg->width + 2, y + msg->height + 2, 1.0f, bordercolour, 1.0f - spa8);
 #endif
 
-					gdl = textRenderProjected(gdl, &x, &y, msg->text, msg->font1, msg->font2, textcolour, viGetWidth(), viGetHeight(), 0, 0);
+					if (spa8 < 1.0f) {
+						gdl = textRenderProjected(gdl, &x, &y, msg->text, msg->font1, msg->font2, textcolour, viGetWidth(), viGetHeight(), 0, 0);
+					}
 				} else {
 					gdl = text0f153a34(gdl, x, y, x + msg->width, y + msg->height, 0);
 
